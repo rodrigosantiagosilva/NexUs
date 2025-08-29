70f302a4c428db6722ce320fc9d97551119b663d
Rodrigo <160535589+rodrigosantiagosilva@users.noreply.github.com>
2025-08-29 09:08:09 -0300

    CU
    CU

--- hub.php
+++ hub.php
@@ -244,7 +244,7 @@ $msgsPorGrupo = $stmt->fetchAll(PDO::FETCH_KEY_PAIR);
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <title>Hub - TydraPI</title>
+  <title>CU</title>
 
   <!-- Bootstrap -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
